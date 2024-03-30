760089ba2b2fc02e0753c0303d9349cd803a665e
zerrudop <94140890+zerrudop@users.noreply.github.com>
2024-03-30 19:35:32 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const magazines = [
+  {id:1, title:'Architectural Digest', theme:'architecture', isAvailable: true},
+  {id:2, title:'Dwell', theme:'architecture', isAvailable: true},
+  {id:3, title:'Communication Arts', theme:'design', isAvailable: false}
+];
+
+function ZineRack(){
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'green' : 'red'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return(
+    <ul>{listZines}</ul>
+  )
+}
+
+const book = {
+  title: 'Diary of a Wimpy Kid',
+  author: 'Jeff Kinney',
+  published: '2007',
+  image: 'https://3.bp.blogspot.com/-vWm8B0jTtGw/UNBMOU1px0I/AAAAAAAACBg/lr40GFfNm7w/s1600/Diary+of+a+Wimpy+Kid+Book+1_ABRAMS.JPG',
+  width: '100',
+  height: '100'
+};
+
+function Bookshelf(){
+  return(
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img
+        className='bookCover'
+        src= {book.image}
+        alt= {book.title +'cover'}
+        style={{
+          width:book.width,
+          height:book.height
+        }}
+      />
+      
+    </div>
+  );
+}
+
+function MagicButton(){
+  return(
+    <>
+      <h3>This is a magic button</h3>
+      <button>Magic</button>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src="linkedin.jpg" alt = ""/>
-        <p>
-          Edit <code>spaghetti</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am in pain!!!
-        </a>
+        <ZineRack/>
+        <Bookshelf/>
+        <MagicButton/>
       </header>
+      
     </div>
   );
 }
